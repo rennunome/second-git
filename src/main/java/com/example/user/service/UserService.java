43a3829926449b4650aa709316aa4f9ccb377ebd
rennunome <r_nunome@emyus.jp>
2021-03-16 18:25:12 +0900

    Modified LoginController.java, application.properties, generationConfig.xml

---
+++ src/main/java/com/example/user/service/UserService.java
@@ -0,0 +1,32 @@
+package com.example.user.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.user.domain.User;
+import com.example.user.repository.UserRepository;
+
+@Service
+public class UserService {
+    // ①
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User findOne(int id) {
+        return userRepository.findOne(id);
+    }
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    public void delete(int id) {
+        UserRepository.delete(id);
+    }
+}
